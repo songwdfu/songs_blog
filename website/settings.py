8da1e886e3e2103de292a215c82dcf63a0dd105d
SongGns <SongGns@users.noreply.github.com>
2024-06-09 17:27:31 +0800

    add uwsgi test

--- website/settings.py
+++ website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-^cdjd13^*6ovj=8rk6v165x3p*kfv)_ebg)bkglub!cmsn^3i+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["20.205.130.91", "localhost"]
 
 
 # Application definition
